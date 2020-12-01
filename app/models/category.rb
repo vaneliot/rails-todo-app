313634ddf7521b322fea9ed04a8d2701ecf215b1
Van <van.eliot.santos@gmail.com>
2020-12-01 18:04:00 +0800

    Improved layout. Added some links to establish better connection between Categories & Tasks.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :description, presence: false
 
-    has_many :tasks #, dependent: :destroy
+    has_many :tasks, dependent: :destroy
 end
